7e2b418c49bc061884ff03bd6fb68ad37fc0444f
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 11:18:50 +0530

    Program-4 Completed

--- program-4.js
+++ program-4.js
@@ -1,1 +1,30 @@
 // Write a JavaScript program that creates a class called "BankAccount" with properties for account number and balance. Include methods to deposit and withdraw money from the account. Create some instances of the "BankAccount" class, deposit some money, and withdraw a portion of it.
+
+class Account {
+    constructor(accountNumber, initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+    
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            console.log(`Deposited ${amount} into your account ${this.accountNumber}. New balance: ${this.balance}`);
+        } else {
+            console.log("Invalid deposit amount.");
+        }
+    }
+    
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`Withdraw ${amount} from your account ${this.accountNumber}. New balance: ${this.balance}`);
+        } else {
+            console.log("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
+}
+
+const account = new Account("123456789", 1000);
+account.deposit(500);
+account.withdraw(200);
