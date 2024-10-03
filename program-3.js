1341cc1cd7df2a53857299dcf697e09b19b1355f
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 11:09:24 +0530

    Program-3 Completed

--- program-3.js
+++ program-3.js
@@ -1,1 +1,26 @@
 // Write a JavaScript program that creates a class called 'Vehicle' with properties for make, model, and year. Include a method to display vehicle details. Create a subclass called 'Car' that inherits from the 'Vehicle' class and includes an additional property for the number of doors. Override the display method to include the number of doors.
+
+class Vehicle{
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    display(){
+        console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+    }
+}
+
+class Car extends Vehicle{
+    constructor(make, model, year, doors){
+        super(make, model, year);
+        this.doors = doors;
+    }
+    display(){
+        console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year} , Number of Doors: ${this.doors}`);
+    }
+}
+
+let myCar = new Car("Toyota", "Camry", 2020, 4);
+
+myCar.display(); 
