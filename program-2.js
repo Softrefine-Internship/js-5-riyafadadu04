c6c5e4bb990778190c13f2dc42c47295ee04335b
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 11:01:20 +0530

    Program-2 Completed

--- program-2.js
+++ program-2.js
@@ -1,2 +1,24 @@
 //  Write a JavaScript program to create a class called 'Rectangle' with properties for width and height. Include two methods to calculate rectangle area and perimeter. Create an instance of the 'Rectangle' class and calculate its area and perimeter.
 
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    
+    AreaOfRectangle(){
+        return (this.width * this.height).toFixed(2);
+    }
+
+    PerimeterOfRectangle(){
+        return (2 * (this.width + this.height)).toFixed(2);
+    }
+}
+
+// Create an instance of the Rectangle class
+const rectangle1 = new Rectangle(8.1, 10.222);
+const rectangle2 = new Rectangle(10.2,20.5);
+
+// Calculate area and perimeter
+console.log(`Rectange1 Area: ${rectangle1.AreaOfRectangle()} , Rectange1 Perimeter: ${rectangle1.PerimeterOfRectangle()}`);
+console.log(`Rectange2 Area: ${rectangle2.AreaOfRectangle()} , Rectange2 Perimeter: ${rectangle2.PerimeterOfRectangle()}`);
